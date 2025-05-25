1a8ad0c17b776f71923e344d1a4bd3b1eefcd772
Matheus <matheus.meireles@pditabira.com>
2025-05-25 12:03:35 -0300

    Delete modulo4 directory

--- modulo4/aula1_questao1.py
+++
@@ -1,9 +0,0 @@
-#Transforme em código o fluxograma a seguir
-
-x = int (input("Digite um numero: "))
-if x > 5:
-    print("Maior que 5")
-    print("FIM!")
-else:
-    print("FIM!")
-    
--- modulo4/aula1_questao2.py
+++
@@ -1,8 +0,0 @@
-#Transforme em código o fluxograma a seguir
-
-n = int (input("Digite um numero: "))
-cont = 0
-while n > cont:
-    cont = cont + 1
-    print(cont)
-print ("FIM!")
