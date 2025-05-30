c4d8657074e0143bf8cadf873008e0dce59e60fa
Matheus <matheus.meireles@pditabira.com>
2025-05-29 21:23:21 -0300

    Delete modulo5 directory

--- modulo5/aula 1/aula1_questao1.py
+++
@@ -1,8 +0,0 @@
-#Desenvolva um programa em Python que peça ao usuário para inserir dois números decimais e calcule a diferença absoluta entre esses dois números. 
-# Utilize a função nativa abs para garantir que o resultado seja sempre positivo e round para arredondar o resultado para duas casas decimais.
-
-n1 = float (input("Digite o primeiro número: "))
-n2 = float (input("Digite o segundo número: "))
-diferenca = abs (n1 - n2 )
-diferenca = round(diferenca, 2)
-print ("A diferença absoluta de dois numeros é :",diferenca)
